0276e70ac9370f37cb0ecd1d6d8b2029a3286825
prschulz <prschulz88@gmail.com>
2014-12-01 13:44:05 -0800

    solution to ip_address validation. Tests pass. Kablam.

--- lib/ip_address.rb
+++ lib/ip_address.rb
@@ -1,1 +1,22 @@
 # ip address validator here
+
+def valid_ip?(ip)
+  ip.to_s # Make sure it is a string
+  valid_ip = ip.split('.') # split it on the pd
+
+  if valid_ip.length == 4
+    valid_ip.each do |num|
+      number = num.to_i # convert it to a number
+
+      if number < 0 || number > 255
+        return false
+      end #end if
+
+    end #end loop
+    return true #if through loop and false not hit return true
+
+  else
+    return false #if length not 4
+  end #end length if
+end
+
